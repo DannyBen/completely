986f4d1eb8652061e23e8c73301989b021c53482
Danny Ben Shitrit <db@dannyben.com>
2022-05-19 15:55:13 +0000

    - Add Tester class for testing any completions script

---
+++ lib/completely/tester.rb
@@ -0,0 +1,48 @@
+require 'erb'
+require 'tempfile'
+
+module Completely
+  class Tester
+    attr_reader :script, :script_path, :function_name, :cword, :compline
+
+    def initialize(script: nil, script_path: nil, function_name: )
+      @script, @script_path, @function_name = script, script_path, function_name
+    end
+
+    def test(compline)
+      Tempfile.create "completely-tester" do |f|
+        f << tester_script(compline)
+        f.flush
+        `bash #{f.path}`
+      end.split " "
+    end
+
+    def tester_script(compline)
+      set_compline_vars compline
+      ERB.new(template, trim_mode: '%-').result(binding)
+    end
+
+  protected
+
+    def set_compline_vars(compline)
+      @compline = compline
+      @cword = compline.split(' ').size - 1
+      @cword += 1 if compline.end_with? ' '
+    end
+
+    def absolute_script_path
+      @absolute_script_path ||= begin
+        script_path ? File.expand_path(script_path) : nil
+      end
+    end
+
+    def template_path
+      @template_path ||= File.expand_path "tester-template.erb", __dir__
+    end
+
+    def template
+      @template ||= File.read template_path
+    end
+
+  end
+end
