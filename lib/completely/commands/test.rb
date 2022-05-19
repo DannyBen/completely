609030eca74be8b0011d2e9dd72e407da7a144f7
Danny Ben Shitrit <db@dannyben.com>
2022-05-19 17:59:57 +0000

    make COMPLETELY_SCRIPT_FUNCTION optional

--- lib/completely/commands/test.rb
+++ lib/completely/commands/test.rb
@@ -10,7 +10,7 @@ module Completely
 
         In order to test on a completely configuration file other than 'completely.yaml', set the COMPLETELY_CONFIG_PATH environemnt variable.
 
-        In order to test on an arbitrary completions script, set the COMPLETELY_SCRIPT_PATH and COMPLETELY_SCRIPT_FUNCTION environment variables.
+        In order to test on an arbitrary completions script, set the COMPLETELY_SCRIPT_PATH and optionally the COMPLETELY_SCRIPT_FUNCTION environment variables.
       EOF
 
       usage "completely test COMPLINE"
@@ -20,14 +20,13 @@ module Completely
 
       environment "COMPLETELY_CONFIG_PATH", "Path to a completely configuration file [default: completely.yaml]"
       environment "COMPLETELY_SCRIPT_PATH", "Path to a completions script. When set, this script will be tested instead of the completely configuration file"
-      environment "COMPLETELY_SCRIPT_FUNCTION", "The main completion function to call when using a custom script"
+      environment "COMPLETELY_SCRIPT_FUNCTION", "The main completion function to call when using a custom script. If not set, the basename of the script path will be used, prefixed by an underscore"
 
       example %q[completely test "mygit pu"]
       example %q[completely test "mygit pull "]
       example <<~EOF
-        COMPLETELY_SCRIPT_PATH=/usr/share/bash-completion/completions/git \\
-          COMPLETELY_SCRIPT_FUNCTION=_git \\
-          completely test "git pu"
+        COMPLETELY_SCRIPT_PATH=/usr/share/bash-completion/completions/chown \\
+          completely test "chown --"
         EOF
 
       attr_reader :config, :script_path, :script_function
@@ -48,16 +47,16 @@ module Completely
           completions = Completions.load config
           completions.tester
         else
-          Tester.new script: File.read(script_path), function_name: ENV['COMPLETELY_SCRIPT_FUNCTION']
+          Tester.new script_path: script_path, function_name: script_function
         end
       end
 
       def set_vars
         if ENV['COMPLETELY_CONFIG_PATH']
           @config = ENV['COMPLETELY_CONFIG_PATH']
-        elsif ENV['COMPLETELY_SCRIPT_PATH'] and ENV['COMPLETELY_SCRIPT_FUNCTION']
+        elsif ENV['COMPLETELY_SCRIPT_PATH']
           @script_path = ENV['COMPLETELY_SCRIPT_PATH']
-          @script_function = ENV['COMPLETELY_SCRIPT_FUNCTION']
+          @script_function = ENV['COMPLETELY_SCRIPT_FUNCTION'] || "_#{File.basename(script_path)}"
         elsif File.exist? 'completely.yaml'
           @config = 'completely.yaml'
         else
