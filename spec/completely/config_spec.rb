21bc5d4c5f33b1e63c75026307d2dd83212fbd79
Danny Ben Shitrit <db@dannyben.com>
2025-03-24 12:28:54 +0000

    add complete_options specs

--- spec/completely/config_spec.rb
+++ spec/completely/config_spec.rb
@@ -9,4 +9,20 @@ describe Config do
       expect(subject.flat_config.to_yaml).to match_approval('config/flat_config')
     end
   end
+
+  context 'when complete_options is defined' do
+    let(:file) { 'complete_options' }
+
+    describe 'config' do
+      it 'ignores the completely_config YAML key' do
+        expect(subject.config.keys).to eq ["mygit"]
+      end
+    end
+
+    describe 'options' do
+      it 'returns complete_options from the YAML file' do
+        expect(subject.options[:complete_options]).to eq '-o nosort'
+      end
+    end
+  end
 end
